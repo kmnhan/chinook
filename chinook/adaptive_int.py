693b1586b4fe425edcae0ca5e957ccdc18cb405a
Kimoon Han <khan@kaist.ac.kr>
2024-06-27 17:27:52 +0900

    Revert "feat: use exact coulomb wave function"
    This reverts commit d1d0419639b5d4278a934c3e7f94390af3a0111c.

--- chinook/adaptive_int.py
+++ chinook/adaptive_int.py
@@ -42,20 +42,9 @@
 
 
 import scipy.special as sc
-import numpy as np
-import mpmath as mp
-
-_coulombf = np.frompyfunc(mp.coulombf, 3, 1)
 
 
-def coulombf(n, eta, x):
-    out = _coulombf(n, eta, x)
-    if np.iterable(out):
-        return out.astype(np.complex128)
-    else:
-        return np.complex128(out)
-
-def general_Bnl_integrand(func,kn,lp,sf_factor):
+def general_Bnl_integrand(func,kn,lp):
     
     '''
     Standard form of executable integrand in the e.r approximation of the matrix element
@@ -76,14 +65,9 @@ def general_Bnl_integrand(func,kn,lp,sf_factor):
     '''
     
     def lambda_gen():
-        def fn(r):
-
-            return(-1.0j)**lp*r**3*(coulombf(lp,sf_factor,kn*r)/(kn*r))*func(r)
-        
-        return fn
+        return lambda r: (-1.0j)**lp*r**3*sc.spherical_jn(lp,r*kn)*func(r)
     
     return lambda_gen()
-
 
 def rect(func,a,b):
     
